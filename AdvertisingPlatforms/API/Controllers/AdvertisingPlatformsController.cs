d32bb4a76c5c3054b332862983aadef178dfa1d9
Andrey <cibirkan@gmail.com>
2025-03-10 22:06:19 +0300

    Create plugs for apis

---
+++ AdvertisingPlatforms/API/Controllers/AdvertisingPlatformsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdvertisingPlatforms.API.Controllers;
+
+[Route("api/v1/[controller]")]
+[ApiController]
+public class AdvertisingPlatformsController : ControllerBase
+{
+    // TODO: прописать используемые сервисы
+    
+    public AdvertisingPlatformsController()
+    {
+        // TODO: инициализировать сервисы
+    }
+
+    [Route("ad_platforms_from_file")]
+    [HttpGet]
+    public async Task<ActionResult> LoadingAdPlatformsFromFile()
+    {
+        // TODO: вызов сервиса для загрузки данных о рекламных
+        return Ok();
+    }
+
+    [Route("ad_platforms/{location}")]
+    [HttpGet]
+    public async Task<ActionResult> FindAdPlatformsByLocation(string location)
+    {
+        // TODO: вызов сервиса для поиска рекламных площадок по локации
+        return Ok();
+    }
+}
