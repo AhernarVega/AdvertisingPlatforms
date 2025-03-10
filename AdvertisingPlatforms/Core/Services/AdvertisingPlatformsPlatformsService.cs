cef612811f011190687cde35e12d9389e78d04e5
Andrey <cibirkan@gmail.com>
2025-03-10 22:21:04 +0300

    Create plugs for methods in service

---
+++ AdvertisingPlatforms/Core/Services/AdvertisingPlatformsPlatformsService.cs
@@ -0,0 +1,25 @@
+﻿namespace AdvertisingPlatforms.Core.Services;
+
+public class AdvertisingPlatformsPlatformsService
+{
+    // TODO: объявление необходимых зависимостей
+    
+    public AdvertisingPlatformsPlatformsService()
+    {
+        // TODO: инициализация необходимых зависимостей
+    }
+
+    public async Task<List<string>> LoadingAdPlatformsFromFileAsync()
+    {
+        // TODO: изменить возвращаемый тип на список рекламных площадок
+        // TODO: реализовать загрузку списка рекламных площадок
+        return [];
+    }
+
+    public async Task<List<string>> FindAdPlatformsByLocationAsync(string location)
+    {
+        // TODO: изменить возвращаемый тип на список рекламных площадок
+        // TODO: реализовать получение списка рекламных площадок для заданной локации
+        return [];
+    }
+}
