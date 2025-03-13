a0c83b114a0d54412d378f06fb8e77df4b416b52
Andrey <cibirkan@gmail.com>
2025-03-13 22:37:23 +0300

    Create a response dto for exceptions

---
+++ AdvertisingPlatforms/Presentation/Dtos/Responses/ExceptionResponse.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+
+namespace AdvertisingPlatforms.Presentation.Dtos.Responses;
+
+public record ExceptionResponse
+{
+    public HttpStatusCode StatusCode { get; set; }
+    public string Description { get; set; }
+
+    public ExceptionResponse(HttpStatusCode statusCode, string description)
+    {
+        StatusCode = statusCode;
+        Description = description;
+    }
+}
