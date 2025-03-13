72c0448c240b037cfe57cf17251e7ff38989d403
Andrey <cibirkan@gmail.com>
2025-03-13 22:38:26 +0300

    Create an input data validator for the request

---
+++ AdvertisingPlatforms/Presentation/Validators/LocationRequestValidator.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+using FluentValidation;
+
+namespace AdvertisingPlatforms.Presentation.Validators;
+
+public partial class LocationRequestValidator : AbstractValidator<string>
+{
+    public LocationRequestValidator()
+    {
+        RuleFor(x => x)
+            .NotNull().WithMessage("Location can't be null")
+            .NotEmpty().WithMessage("Location can't be empty")
+            .Must(x => 
+                LocationRequestRegex().Match(x).Success).WithMessage("The location has an incorrect format");
+    }
+
+    [GeneratedRegex(@"^(/[\w_-]+)+$")]
+    private static partial Regex LocationRequestRegex();
+}
